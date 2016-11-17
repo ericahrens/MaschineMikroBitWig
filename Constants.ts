e14df502b8d42cded899073951aedd0d6e6e8b1b
Eric Ahrens <eric.n.ahrens@googlemail.com>
2016-11-17 12:59:59 +0100

    Changed to Indexed Colors

---
+++ Constants.ts
@@ -0,0 +1,103 @@
+let NoteStr: string[] = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+
+class ColorTable {
+
+    _fixColorTable: { [key: number]: number } = {
+        13016944: 16,
+        5526612: 68,
+        8026746: 68,
+        13224393: 68,
+        8817068: 52,
+        10713411: 12,
+        5726662: 48,
+        8686304: 48,
+        9783755: 52,
+        14235761: 60,
+        14233124: 4,
+        16733958: 8,
+        14261520: 16,
+        7575572: 20,
+        40263: 28, // <*>
+        42644: 32,
+        39385: 44,
+        12351216: 52,
+        14771857: 64,
+        15491415: 12,
+        16745278: 12,
+        14989134: 16,
+        10534988: 24,
+        4111202: 32,
+        4444857: 36,
+        4507903: 40,
+        8355711: 68
+    }
+
+    _colorTable: { [key: number]: number } = {};
+
+    convertColor(red, green, blue) : number {
+        if (red === 0 && green === 0 && blue === 0) {
+            return 0;
+        }
+        var rv = Math.floor(red * 255);
+        var gv = Math.floor(green * 255);
+        var bv = Math.floor(blue * 255);
+        var lookupIndex = rv << 16 | gv << 8 | bv;
+
+        if (lookupIndex in this._fixColorTable) {
+            return this._fixColorTable[lookupIndex];
+        }
+        if (lookupIndex in this._colorTable) {
+            return this._colorTable[lookupIndex];
+        }
+
+        var hsb = this.rgbToHsb(rv, gv, bv);
+        var hue = hsb[0];
+        var sat = hsb[1];
+        var bright = hsb[2];
+
+        if (bright < 1 || sat < 3) {
+            println(lookupIndex + ": " + 68 + ",");
+            this._colorTable[lookupIndex] = 68;
+            return this._colorTable[lookupIndex];
+        }
+        var off = 0;
+        if ((bright + sat) < 22) {
+            off = 1;
+        }
+        if (2 <= hue && hue <= 6 && bright < 13) {
+            off = 2;
+        }
+        var color_index = Math.min(hue + off + 1, 16);
+        var color = color_index << 2;
+        this._colorTable[lookupIndex] = color;
+        println(lookupIndex + " : " + color + ",");
+        return color;
+    }
+
+    rgbToHsb(rv, gv, bv) : number[] {
+        var rgb_max = Math.max(Math.max(rv, gv), bv);
+        var rgb_min = Math.min(Math.min(rv, gv), bv);
+        var bright = rgb_max;
+        if (bright === 0) {
+            return [0, 0, 0]; // Dark Dark Black
+        }
+        var sat = 255 * (rgb_max - rgb_min) / bright;
+        if (sat === 0) {
+            return [0, 0, 0]; // White
+        }
+        var hue = 0;
+        if (rgb_max === rv) {
+            hue = 0 + 43 * (gv - bv) / (rgb_max - rgb_min);
+        }
+        else if (rgb_max === gv) {
+            hue = 85 + 43 * (bv - rv) / (rgb_max - rgb_min);
+        }
+        else {
+            hue = 171 + 43 * (rv - gv) / (rgb_max - rgb_min);
+        }
+        if (hue < 0) {
+            hue = 256 + hue;
+        }
+        return [Math.floor(hue / 16.0 + 0.3), sat >> 4, bright >> 4];
+    } 
+}
--- MaschineMikro.control.ts
+++ MaschineMikro.control.ts
@@ -1,27 +1,29 @@
 
 loadAPI(1);
 load("NoteView.js")
+load("Constants.js")
 
 host.defineController("Native Instruments", "Maschine Mikro MK2", "1.0",
-		"749beff0-f8d8-4e7f-8328-011625bd1b99", "Eric Ahrens")
+	"749beff0-f8d8-4e7f-8328-011625bd1b99", "Eric Ahrens")
 host.defineMidiPorts(1, 1);
-host.addDeviceNameBasedDiscoveryPair([ "Maschine Mikro MK2 In" ],
-		[ "Maschine Mikro MK2 Out" ])
+host.addDeviceNameBasedDiscoveryPair(["Maschine Mikro MK2 In"],
+	["Maschine Mikro MK2 Out"])
 
 let __updateQueue = []
 let __actions = 0
 let __controllers: Controllers
+let __colorTable : ColorTable  = new ColorTable()
 
-enum MIDI  {
-    CC = 11,
-    NOTE = 9,
+enum MIDI {
+	CC = 11,
+	NOTE = 9,
 	PAT = 10
 }
 
-function init() : void {
+function init(): void {
 	// Consider separate Noteins
 	let noteIn = host.getMidiInPort(0).createNoteInput("Maschine Mikro MK2 In",
-			"81????", "90????", "B1????", "D1????", "E1????", "A0????")
+		"81????", "90????", "B1????", "D1????", "E1????", "A0????")
 	host.getMidiInPort(0).setMidiCallback(onMidi)
 	noteIn.setShouldConsumeEvents(false)
 
@@ -32,25 +34,25 @@ function init() : void {
 	let transport = new Transport()
 	let modeHandler = new GeneralModeHandler()
 
-	let trackView = host.createCursorTrack(null,8,8)
-	
+	let trackView = host.createCursorTrack(null, 8, 8)
+
 
 	host.scheduleTask(handleSurfaceUpdate, null, 1)
 	println(" ###### Maschine with Typescript ######## ")
 	__controllers.buttonMatrix.init()
 }
 
-function handleSurfaceUpdate() : void {
+function handleSurfaceUpdate(): void {
 	__actions = 10;
 	while (__updateQueue.length > 0 && __actions > 0) {
 		var message = __updateQueue.shift();
 		host.getMidiOutPort(0).sendMidi(message[0], message[1], message[2]);
 		__actions--;
 	}
-    host.scheduleTask(handleSurfaceUpdate, null,1)
+	host.scheduleTask(handleSurfaceUpdate, null, 1)
 }
 
-function queueMidi(status, data1, data2) : void {
+function queueMidi(status, data1, data2): void {
 	if (data2 === undefined) {
 		println(" Undefined Midi Data ");
 		return;
@@ -62,22 +64,22 @@ function queueColor(color: number, control: number, value: number) {
 	__updateQueue.push([0xB0 | color, control, value]);
 }
 
-function onMidi(status: number, data1: number, data2: number) : void {  
-	if(__controllers.handleMidi(status,data1,data2)) {
-	    println(" Got Unmapped Midi  " + status + " " + data1 + " " + data2)
+function onMidi(status: number, data1: number, data2: number): void {
+	if (__controllers.handleMidi(status, data1, data2)) {
+		println(" Got Unmapped Midi  " + status + " " + data1 + " " + data2)
 	}
 }
 
 class GeneralModeHandler {
-	private viewButton : Button
-	private padButton : Button
-	private scenButton : Button
-	private patternButton : Button
-	private groupButton : BasicColorButton
+	private viewButton: Button
+	private padButton: Button
+	private scenButton: Button
+	private patternButton: Button
+	private groupButton: BasicColorButton
 
-	private shiftButton : Button
+	private shiftButton: Button
 
-	public shiftDown : boolean = false;
+	public shiftDown: boolean = false;
 
 	constructor() {
 		this.viewButton = __controllers.createButton(115)
@@ -93,18 +95,18 @@ class GeneralModeHandler {
 		})
 
 		this.groupButton.setCallback((value, button) => {
-			if(value > 0) {
-				this.groupButton.updateColor(5,127,127)
+			if (value > 0) {
+				this.groupButton.sendValue(30)
 			} else {
-				this.groupButton.updateColor(5,127,50)
+				this.groupButton.sendValue(0)
 			}
 		})
 	}
 }
 
 class Transport {
-	private playButton : Button
-	
+	private playButton: Button
+
 	constructor() {
 		this.playButton = __controllers.createButton(108);
 
@@ -117,64 +119,96 @@ class Transport {
 
 class Controllers {
 	private noteMapping = {}
-	private controls : {[ccNr: number] : Button} = {};
-	buttonMatrix : ButtonMatrix
+	private controls: { [ccNr: number]: Button } = {};
+	buttonMatrix: ButtonMatrix
 
 	constructor() {
 		this.buttonMatrix = new ButtonMatrix(this, 12);
 	}
 
-	createButton(ccNr : number) : Button {
+	createButton(ccNr: number): Button {
 		let button = new Button(ccNr);
 		this.controls[ccNr] = button;
 		return button;
 	}
 
-	createBasicColorbutton(ccNr: number) : BasicColorButton {
+	createBasicColorbutton(ccNr: number): BasicColorButton {
 		let button = new BasicColorButton(ccNr);
 		this.controls[ccNr] = button;
 		return button;
 	}
 
-	fullUpdate() : void {
-		for(let ccNr in this.controls) {
+	fullUpdate(): void {
+		for (let ccNr in this.controls) {
 			this.controls[ccNr].resendLast();
 		}
 	}
 
-	handleMidi(status: number, data1: number, data2: number) : boolean {
+	handleMidi(status: number, data1: number, data2: number): boolean {
 		let channel = status & 0xF
 		let type = status >> 4
-		switch(type) {
+		switch (type) {
 			case 0xA: // Poly Aftertouch
 				break;
 			case 0xB: // MIDI CC
-				if(data1 in this.controls) {
+				if (data1 in this.controls) {
 					this.controls[data1].action(data2)
 					return false;
 				} else {
 					return true;
 				}
 			case 0x9: // MIDI Note
-				if(data1 > 11 && data1 <29) {
-					let index = data1-12
-					this.buttonMatrix.setBrightness(index, data2 > 0 ? 127 : 0)
+				if (data1 > 11 && data1 < 29) {
+					let index = data1 - 12
+					this.buttonMatrix.setColor(index, data2 > 0 ? 10 : 0)
 					return false
 				}
 				break;
-		} 
+		}
 		return false
 	}
 }
 
 interface Controller {
-	sendValue(value: number) :void
-	reset() : void 
+	sendValue(value: number): void
+	reset(): void
+}
+
+
+class ColorIndexButton {
+	private parent: ButtonMatrix
+	private lastvalue: number = 0
+	private index: number
+	private row: number
+	private col: number
+	private base: number
+	private midistatus: number
+
+	constructor(parent: ButtonMatrix, index: number, base: number) {
+		this.index = 0
+		this.base = base;
+		this.col = this.index % 4
+		this.row = Math.floor(this.index / 4)
+		this.parent = parent
+		this.midistatus = MIDI.NOTE << 4 | 0;
+	}
+
+	sendValue(value: number): void {
+		if (value != this.lastvalue) {
+			this.lastvalue = value
+			queueMidi(this.midistatus, this.base, value);
+		}
+	}
+
+	update(force: boolean = false): void {
+		queueMidi(this.midistatus, this.base, this.lastvalue);
+	}
+
 }
 
 class ColorButton {
 	private parent: ButtonMatrix
-	private hue : number = 50
+	private hue: number = 50
 	private sat: number = 127
 	private bright: number = 0
 	private index: number
@@ -190,43 +224,43 @@ class ColorButton {
 		this.parent = parent
 	}
 
-	updateColor( hue: number, sat: number, bright: number) : void {
-		if(hue != this.hue) {
+	updateColor(hue: number, sat: number, bright: number): void {
+		if (hue != this.hue) {
 			this.hue = hue
 			queueColor(0, this.base, this.hue)
 		}
-		if(sat != this.sat) {
+		if (sat != this.sat) {
 			this.sat = sat
 			queueColor(1, this.base, this.sat)
 		}
-		if(bright != this.bright) {
+		if (bright != this.bright) {
 			this.bright = bright
 			queueColor(2, this.base, this.bright)
 		}
 	}
 
 	updateBright(value: number) {
-		if(value != this.bright) {
+		if (value != this.bright) {
 			this.bright = value
 			queueColor(2, this.base, this.bright)
 		}
 	}
 
 	updateHue(value: number) {
-		if(value != this.hue) {
+		if (value != this.hue) {
 			this.hue = value
 			queueColor(0, this.base, this.hue)
 		}
 	}
 
 	updateSat(value: number) {
-		if(value != this.sat) {
+		if (value != this.sat) {
 			this.sat = value
 			queueColor(1, this.base, this.sat)
 		}
 	}
 
-	update(force : boolean = false) : void {
+	update(force: boolean = false): void {
 		queueColor(0, this.base, this.hue)
 		queueColor(1, this.base, this.sat)
 		queueColor(2, this.base, this.bright)
@@ -235,34 +269,39 @@ class ColorButton {
 
 
 class ButtonMatrix {
-	private parent : Controllers
+	private parent: Controllers
 	private basevalue: number
 	private midistatus: number
-	private buttons : ColorButton[]
+	private buttons: ColorIndexButton[]
 
 	constructor(parent: Controllers, basevalue: number) {
 		this.parent = parent
 		this.basevalue = basevalue
 		this.midistatus = 0x90
 		this.buttons = []
-		for(let i=0;i<16;i++) {
-			this.buttons.push(new ColorButton(this, i, basevalue+i))
+		for (let i = 0; i < 16; i++) {
+			this.buttons.push(new ColorIndexButton(this, i, basevalue + i))
 		}
 	}
 
-	setBrightness(index: number, brightness: number) : void {
-		if(index < 0 || index > 15) {
+	setColor(index: number, value: number): void {
+		this.buttons[index].sendValue(value);
+	}
+
+	setBrightness(index: number, brightness: number): void {
+		if (index < 0 || index > 15) {
 			return
 		}
-		this.buttons[index].updateBright(brightness)
+		//this.buttons[index].updateBright(brightness)
+		this.buttons[index].sendValue(brightness)
 	}
 
-	setColor(index: number, hue: number, sat: number, bright: number) {
-		this.buttons[index].updateColor(hue,sat,bright)
-	}
+	//setColor(index: number, hue: number, sat: number, bright: number) {
+	// this.buttons[index].updateColor(hue,sat,bright)
+	//}
 
-	init() : void {
-		for(let i=0;i<16;i++) {
+	init(): void {
+		for (let i = 0; i < 16; i++) {
 			this.buttons[i].update(true)
 		}
 	}
@@ -270,78 +309,63 @@ class ButtonMatrix {
 
 class Button implements Controller {
 
-	protected midivalue : number;
-	private midistatus: number;
-	private lastValue : number = -1
-	private callback : (value: number, button: Button) => void;
+	protected midivalue: number;
+	protected midistatus: number;
+	private lastValue: number = -1
+	private callback: (value: number, button: Button) => void;
 
 	constructor(midivalue: number, channel: number = 0) {
 		this.midivalue = midivalue;
 		this.midistatus = MIDI.CC << 4 | channel;
 	}
 
-	action(value: number) : void {
-		if(this.callback) {
+	action(value: number): void {
+		if (this.callback) {
 			this.callback(value, this);
 		}
 	}
 
-	setCallback(callback : (value: number, button: Button) => void) {
+	setCallback(callback: (value: number, button: Button) => void) {
 		this.callback = callback;
 	}
 
-	sendValue(value: number) :void {
-		if(this.lastValue != value) {
+	sendValue(value: number): void {
+		if (this.lastValue != value) {
 			queueMidi(this.midistatus, this.midivalue, value)
 		}
 		this.lastValue = value
 	}
 
-	reset() : void {
+	reset(): void {
 		queueMidi(this.midistatus, this.midivalue, 0)
 	}
 
-	resendLast() : void {
-		if(this.lastValue >= 0) {
+	resendLast(): void {
+		if (this.lastValue >= 0) {
 			queueMidi(this.midistatus, this.midivalue, this.lastValue);
 		}
 	}
 }
 
 class BasicColorButton extends Button {
-	private hue : number = 0
-	private sat: number = 0
-	private bright: number = 0
+	private value: number = 0
 
 	constructor(midivalue: number, channel: number = 0) {
 		super(midivalue, channel)
 	}
 
-	updateColor( hue: number, sat: number, bright: number) : void {
-		if(hue != this.hue) {
-			this.hue = hue
-			queueColor(3, this.midivalue, this.hue)
-		}
-		if(sat != this.sat) {
-			this.sat = sat
-			queueColor(4, this.midivalue, this.sat)
-		}
-		if(bright != this.bright) {
-			this.bright = bright
-			queueColor(5, this.midivalue, this.bright)
+
+	sendValue(value: number): void {
+		if (value != this.value) {
+			this.value = value
+			queueMidi(this.midistatus, this.midivalue, this.value);
 		}
 	}
 
-	sendValue(value: number) :void {
-	}
-
-	resendLast() : void {
+	resendLast(): void {
 		this.update();
 	}
 
-	update(force : boolean = false) : void {
-		queueColor(0, this.midivalue, this.hue)
-		queueColor(1, this.midivalue, this.sat)
-		queueColor(2, this.midivalue, this.bright)
+	update(force: boolean = false): void {
 	}
 }
